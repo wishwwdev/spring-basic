4cb8bbb642030ae1ce37bf29ea4af9248d3af499
devlim <ywl428@naver.com>
2024-02-03 15:28:41 +0900

    N.5-2

---
+++ basic/src/main/java/com/woolim/basic/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.woolim.basic.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Entity(name="user")
+@Table(name="user")
+public class UserEntity {
+  @Id
+  private String email;
+  private String password;
+  private String nickname;
+  private String telNumber;
+  private String address;
+  private String addressDetail;
+  private String profileImageUrl;
+}
