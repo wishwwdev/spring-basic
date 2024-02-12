7c9a7657ee873f24f1cd72777c97f60e59b9c7a2
devlim <ywl428@naver.com>
2024-02-12 18:14:03 +0900

    N.12-1

--- basic/src/main/java/com/woolim/basic/config/WebSecurityConfig.java
+++ basic/src/main/java/com/woolim/basic/config/WebSecurityConfig.java
@@ -22,13 +22,12 @@ public class WebSecurityConfig {
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
   // description: Security Filter 설정 변경 //
-  @SuppressWarnings("removal")
   @Bean
   protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
 
     httpSecurity
-      // description: cors 정책 설정 - 기본 설정으로 사용 //
-      .cors(cors ->cors.configure(httpSecurity))
+      // description: cors 정책 설정 - 미사용 //
+      .cors(cors ->cors.disable())
       // description: csrf 보안 설정 - 미사용 //
       .csrf(crsf ->crsf.disable())
       // description: basic authentication 설정 - 미사용 //
@@ -36,18 +35,23 @@ public class WebSecurityConfig {
       // description: session 정책 설정 //
       .sessionManagement(management ->management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       // description: API의 URL 패턴에 따라 인증 여부 지정 //
-      .authorizeHttpRequests()
-      // description: 특정 패턴에 대하여 인증 없이 진행 가능하도록 허용 //
-      .requestMatchers("/", "/auth/match").permitAll()
-      // description: 특정 메서드에 대하여 인증 없이 진행 가능하도록 허용 //
-      .requestMatchers(HttpMethod.GET).permitAll()
-      // description: 특정 메서드에 해당하는 특정 패턴에 대하여 인증없이 진행 가능하도록 허용 //
-      .requestMatchers(HttpMethod.POST, "/sign-in", "/sign-up").permitAll()
-      // description: 나머지 요청에 대하여 인증을 필수로 진행하도록 설정 //
-      .anyRequest().authenticated();
-
-    // description: jwtAuthenticationFilter를 httpSecurity 필터에 추가 //
-    httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+      .authorizeHttpRequests(requests -> {
+        try {
+          requests
+            // description: 특정 패턴에 대하여 인증 없이 진행 가능하도록 허용 //
+            .requestMatchers("/", "/auth/match").permitAll()
+            // description: 특정 메서드에 대하여 인증 없이 진행 가능하도록 허용 //
+            .requestMatchers(HttpMethod.GET).permitAll()
+            // description: 특정 메서드에 해당하는 특정 패턴에 대하여 인증없이 진행 가능하도록 허용 //
+            .requestMatchers(HttpMethod.POST, "/sign-in", "/sign-up").permitAll()
+            // description: 나머지 요청에 대하여 인증을 필수로 진행하도록 설정 //
+            .anyRequest().authenticated();
+        } catch (Exception exception) {
+          exception.printStackTrace();
+        }
+      }) 
+      // description: jwtAuthenticationFilter를 httpSecurity 필터에 추가 //
+      .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return httpSecurity.build();
   }
